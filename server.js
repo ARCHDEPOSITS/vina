af578439c2fb31b53c6e6f95f2a4de62b792e910
ARCHDEPOSITS <ceyeconections@gmail.com>
2024-08-09 21:37:32 +1000

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,33 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-const DISCORD_BOT_TOKEN = 'YOUR_BOT_TOKEN';
-const GUILD_ID = 'YOUR_GUILD_ID';
+// Use your actual bot token and guild ID here
+const DISCORD_BOT_TOKEN = 'MTI3MTM5NTUzNTU1ODgwMzQ5Nw.Gqp_aN.-a25vTBG4xRcZWOhLnMYXYa2Bz_wH9hn5y46i4';
+const GUILD_ID = '1265624772780228619';
 
 app.get('/members', async (req, res) => {
     try {
-        const response = await axios.get(`https://discord.com/api/v10/guilds/${GUILD_ID}/members`, {
-            headers: {
-                'Authorization': `Bot ${DISCORD_BOT_TOKEN}`
-            }
-        });
+        let members = [];
+        let url = `https://discord.com/api/v10/guilds/${GUILD_ID}/members?limit=1000`;
         
-        res.json(response.data);
+        while (url) {
+            const response = await axios.get(url, {
+                headers: {
+                    'Authorization': `Bot ${DISCORD_BOT_TOKEN}`
+                }
+            });
+            
+            members = members.concat(response.data);
+            
+            // Check if there are more members to fetch
+            if (response.headers['x-next'] && response.headers['x-next'] !== 'null') {
+                url = response.headers['x-next'];
+            } else {
+                url = null;
+            }
+        }
+        
+        res.json(members);
     } catch (error) {
         res.status(500).send('Error fetching members');
     }
